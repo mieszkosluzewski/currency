0e2badc100c31d1b6fe6ef32de84964a4cbe66f7
Mieszko Służewski <mieszkosluzewski@gmail.com>
2018-08-23 20:11:46 +0200

    Add model and related migrations

--- currency_info_server/api/models.py
+++ currency_info_server/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class ExchangeRate(models.Model):
+    """
+    Model for currency exchange rate.
+
+    Exchange rate are related to EUR.
+    """
+    date = models.DateTimeField()
+    currency = models.CharField(max_length=3)
+    exchange_rate = models.FloatField()
